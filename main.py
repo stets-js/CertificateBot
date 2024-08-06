8b4278a9581c450918422dc3d1cc01d3d40d5bec
Dmytro Stetsenko <97848708+stets-js@users.noreply.github.com>
2024-08-06 15:41:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,56 +11,65 @@ bot = Bot(token=config.TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
-class CommandState:
-    waiting_for_name = "waiting_for_name"
+# Массив користувачів
+users = [{"name": "test testing", "email": "test@gmail.com"}]
 
+class CommandState:
+    waiting_for_email = "waiting_for_email"
 
 @dp.message_handler(Command("start"))
 async def start(message: types.Message):
-    user_id = message.from_user.id
-    user_name = message.from_user.full_name
-    user_username = message.from_user.username
-
     await message.answer(
         "Введи команду /create, щоб створити сертифікат GoITeens"
     )
 
 @dp.message_handler(Command("create"))
 async def create(message: types.Message, state: FSMContext):
-    await message.answer("Введіть своє ім'я, щоб згенерувати сертифікат!")
+    await message.answer("Введіть свою електронну пошту, щоб згенерувати сертифікат!")
+    await state.set_state(CommandState.waiting_for_email)
 
-    await state.set_state(CommandState.waiting_for_name)
+@dp.message_handler(state=CommandState.waiting_for_email)
+async def handle_email(message: types.Message, state: FSMContext):
+    email = message.text
 
-@dp.message_handler(state=CommandState.waiting_for_name)
-async def send_certificate(message: types.Message, state: FSMContext):
-    clock = await bot.send_message(message.chat.id, "⏳")
+    # Перевірка електронної пошти
+    user = next((u for u in users if u["email"] == email), None)
     
-    await asyncio.sleep(1)
-
-    with Image.open("template.png") as im:
-        draw = ImageDraw.Draw(im)
-        font_path = "OpenSans-SemiBold.ttf"
-        font_size = 75
-        font = ImageFont.truetype(font_path, font_size)
+    if user:
+        user_name = user["name"]
         
-        # Використовуйте textbbox для отримання розміру тексту
-        text_bbox = draw.textbbox((0, 0), message.text, font=font)
-        text_width = text_bbox[2] - text_bbox[0]
-        text_height = text_bbox[3] - text_bbox[1]
-        image_width, image_height = im.size
-        text_position = ((image_width - text_width) // 2, 1233)
+        # Генерація сертифікату
+        clock = await bot.send_message(message.chat.id, "⏳")
         
-        draw.text(text_position, message.text, font=font)
+        await asyncio.sleep(1)
 
-        image_buffer = io.BytesIO()
-        im.save(image_buffer, format="PNG")
-        image_buffer.seek(0)
+        with Image.open("template.png") as im:
+            draw = ImageDraw.Draw(im)
+            font_path = "OpenSans-SemiBold.ttf"
+            font_size = 75
+            font = ImageFont.truetype(font_path, font_size)
+            
+            # Використовуйте textbbox для отримання розміру тексту
+            text_bbox = draw.textbbox((0, 0), user_name, font=font)
+            text_width = text_bbox[2] - text_bbox[0]
+            text_height = text_bbox[3] - text_bbox[1]
+            image_width, image_height = im.size
+            text_position = ((image_width - text_width) // 2, 1233)
+            
+            draw.text(text_position, user_name, font=font)
 
-    await asyncio.sleep(1)
+            image_buffer = io.BytesIO()
+            im.save(image_buffer, format="PNG")
+            image_buffer.seek(0)
 
-    await bot.delete_message(message.chat.id, clock.message_id)
-    await message.answer_photo(image_buffer)
-    await state.finish()
+        await asyncio.sleep(1)
+
+        await bot.delete_message(message.chat.id, clock.message_id)
+        await message.answer_photo(image_buffer)
+        await state.finish()
+    else:
+        await message.answer("Пошта не вірна, спробуйте знову.")
+        await state.set_state(CommandState.waiting_for_email)
 
 if __name__ == "__main__":
     import keep_alive
