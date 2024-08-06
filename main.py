004bde96fd60dd6d7aca2fbc508f9d367c86232e
Dmytro Stetsenko <97848708+stets-js@users.noreply.github.com>
2024-08-06 14:17:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import io
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove
 import asyncio
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+# from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
 from PIL import Image, ImageDraw, ImageFont
